1a174be938c0d0fed75027a3bb413679548da1ad
Emmanuel Cobblah <Ecobblah@outlook.com>
2018-06-09 23:31:56 -0400

    added the input sign in and sign up

--- diary.php
+++ diary.php
@@ -1,3 +1,22 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Diary</title>
+  </head>
+  <body>
+    <form method="post">
+      <input type="email" name="email" placeholder="Email">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" name="" value="Sign up">
+
+    <form method="post">
+      <input type="email" name="email" placeholder="Email">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" name="" value="Sign in">
+    </form>
+  </body>
+</html>
