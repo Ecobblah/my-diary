afdceb6e6d87be5d338b4f4808db5c0fd5bca6b0
Emmanuel Cobblah <Ecobblah@outlook.com>
2018-06-11 21:15:25 -0400

    Added in errors when password or email is empty

--- diary.php
+++ diary.php
@@ -1,22 +1,49 @@
 <?php
 
+if($_POST){
+  echo "Thx for your email ".$_POST['email'];
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Diary</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
   <body>
+    <div id="error"></div>
     <form method="post">
-      <input type="email" name="email" placeholder="Email">
-      <input type="password" name="password" placeholder="Password">
-      <input type="submit" name="" value="Sign up">
+      <input class="noEmail" type="email" name="email" placeholder="Email">
+      <input class="noPass" type="password" name="password" placeholder="Password">
+      <input id="signBut" type="submit" name="" value="Sign up">
+      </form>
 
     <form method="post">
-      <input type="email" name="email" placeholder="Email">
+      <input class="noEmail" type="email" name="email" placeholder="Email">
       <input type="password" name="password" placeholder="Password">
-      <input type="submit" name="" value="Sign in">
+      <input id="loginBut"type="submit" name="" value="Sign in">
     </form>
+    <script type="text/javascript">
+      $("#signBut").click(function(){
+        var error="";
+        $("#error").html("");
+        if( $(".noEmail").val()=="" ){
+          error = "<p>Sry you got an empty email</p>";
+          $("#error").html(error);
+        }
+        if( $(".noPass").val()==""){
+          error = error + "<p>Sry you got an empty Password</p>";
+          $("#error").html(error);
+        }
+        if(error ==""){
+          return true;
+        }
+        else {
+          return false;
+        }
+      });
+    </script>
   </body>
 </html>
